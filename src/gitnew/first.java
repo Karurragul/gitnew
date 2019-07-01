ef5b78559249e2ff667f6818a4c02c9fc538c320
karurragul <karurragul@gmail.com>
2019-07-01 06:07:41 +0530

    good

--- src/gitnew/first.java
+++ src/gitnew/first.java
@@ -2,6 +2,12 @@ package gitnew;
 
 public class first {
 	public static void main(String [] args) {
+		System.out.println("++++");
+		System.out.println("Hi Good Morning");
+		System.out.println("___");
+		System.out.println("Hi Good Morning");
+		System.out.println("@@@@");
+		System.out.println("Hi Good Morning");
 		System.out.println("Gokul");
 		System.out.println("Hi Good Morning");
 		System.out.println("Go");
