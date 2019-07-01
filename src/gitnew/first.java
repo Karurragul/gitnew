0f4ef3c2a1c77f708265a540f26b981942fb2e75
karurragul <karurragul@gmail.com>
2019-07-01 06:00:33 +0530

    added

--- src/gitnew/first.java
+++ src/gitnew/first.java
@@ -4,6 +4,10 @@ public class first {
 	public static void main(String [] args) {
 		System.out.println("Gokul");
 		System.out.println("Hi Good Morning");
+		System.out.println("Go");
+		System.out.println("Hi Good Morning");
+		System.out.println("Ragul");
+		System.out.println("Hi Good Morning");
 	}
 
 }
